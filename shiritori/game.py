63087c6b62d8acee6cafadf5f0f3f1015e36c549
Rassilion <denizgezgin365@gmail.com>
2015-08-21 16:45:28 +0300

    Change game attributes
    player data now stored in dict. Move function takes player id

--- shiritori/game.py
+++ shiritori/game.py
@@ -3,7 +3,7 @@
 from datetime import datetime
 import logging
 from itsdangerous import URLSafeTimedSerializer
-from .dictionary import english
+from .dictionary import english, turkish
 import random
 import hashlib
 import uuid
@@ -32,52 +32,59 @@ def md5(data):
 
 
 class Game(object):
-    def __init__(self, id):
-        self.id = id
-        self.p1 = 0
-        self.p2 = 0
-        self.p1_list = []
-        self.p2_list = []
+    def __init__(self, id, dictionary):
+        self.uuid = id
+        self.players = {}
+        # game dictionary
+        self.dictionary = dictionary
+
+        # TODO game types
+        self.mode = None
+        # TODO random start letter?
         self.letter = "a"
+        self.timestamp = datetime.utcnow
+
+    def words(self):
+        w = []
+        for p in self.players:
+            w += self.players[p]['words']
+        return w
 
     def __repr__(self):
-        return str(self.id)
+        return str(self.uuid)
+
+    def add_player(self, id):
+        if id not in self.players:
+            self.players[id] = {'score': 0, 'words': []}
 
     def check(self, word):
-        if word.startswith(self.letter) and word not in self.p1_list and word not in self.p2_list and word in english:
+        print self.words()
+        if word.startswith(
+                self.letter) and word not in self.words() and word in self.dictionary:
             return True
         else:
             return False
 
-    def p1_move(self, word):
+    def player_move(self, id, word):
         if self.check(word):
-            self.p1 += len(word)
+            self.players[id]['score'] += len(word)
             self.letter = word[-1]
-            self.p1_list.append(word)
+            self.players[id]['words'].append(word)
             return True
         else:
             return False
 
-    def p2_move(self, word):
-        if self.check(word):
-            self.p2 += len(word)
-            self.letter = word[-1]
-            self.p2_list.append(word)
-            return True
-        else:
-            return False
-
-    def ai_move(self):
-        word = ""
-        while (not self.check(word)):
-            word = random.choice(english[self.letter])
-        self.p2 += len(word)
-        self.letter = word[-1]
-        self.p2_list.append(word)
+    # def ai_move(self):
+    #     word = ""
+    #     while not self.check(word):
+    #         word = random.choice(english[self.letter])
+    #     self.p2 += len(word)
+    #     self.letter = word[-1]
+    #     self.p2_words.append(word)
 
     # TODO make p1 given userid
     def get_game(self):
-        return {'p1': {'score': self.p1, 'words': self.p1_list}, 'p2': {'score': self.p2, 'words': self.p2_list}}
+        return self.players
 
 
 class ServerConnection(SockJSRoomHandler):
@@ -96,10 +103,10 @@ class ServerConnection(SockJSRoomHandler):
         if self._room.has_key(self._gcls() + _id):
             self._room[self._gcls() + _id].remove(self)
 
-    def create(self, _id):
+    def create(self, _id, dict):
         if not self._room.has_key(self._gcls() + _id):
             self._room[self._gcls() + _id] = set()
-            self._game[self._gcls() + _id] = Game(_id)
+            self._game[self._gcls() + _id] = Game(_id, dict)
         print len(self._game)
 
     def join(self, _id):
@@ -109,6 +116,8 @@ class ServerConnection(SockJSRoomHandler):
             print "room not found"
         else:
             self._room[self._gcls() + _id].add(self)
+            # add player to game
+            self._game[self._gcls() + _id].add_player(self.userid)
 
     def on_open(self, info):
         self.init()
@@ -135,7 +144,11 @@ class ServerConnection(SockJSRoomHandler):
         elif self.token_loader(data["token"]):
             self.roomId = str(data['roomId'])
             # join room
-            self.create(self.roomId)
+            # For test
+            if int(self.roomId) % 2 == 0:
+                self.create(self.roomId, turkish)
+            else:
+                self.create(self.roomId, english)
             self.join(self.roomId)
             # get game of joined room
             self.game = self.getGame(self.roomId)
@@ -149,15 +162,7 @@ class ServerConnection(SockJSRoomHandler):
 
     def on_move(self, data):
         if self.roomId != '-1':
-            if self.userid == '1' and self.game.p1_move(data["move"]):
-                self.publishToRoom(self.roomId, 'move', {
-                    'username': self.userid,
-                    'time': datetime.now(),
-                    'move': str(data['move'])
-                })
-                self.publishToRoom(self.roomId, 'server',
-                                   {'letter': self.game.letter, 'message': "Letter is " + self.game.letter})
-            elif self.game.p2_move(data["move"]):
+            if self.game.player_move(self.userid, data["move"]):
                 self.publishToRoom(self.roomId, 'move', {
                     'username': self.userid,
                     'time': datetime.now(),
